bdc9a2bf2d02ba8d5fc1a3663401fe6935fa7a78
nao-riku <76162238+nao-riku@users.noreply.github.com>
2022-09-28 18:35:49 +0900

    Update app.py

--- app.py
+++ app.py
@@ -206,7 +206,7 @@ def get_stream():
                                                 else:
                                                     rank = "RoR"
                                                     
-                                                rep_text = json_response["includes"]["users"][0]["name"] + "\n\n級位: " + rank + "\n 最高pt: " + world_rank[key][2] + "\n 歴代: " + str(world_rank[key][3]) + " / " + world_rank["累計"][0] + "\n 現在pt: " + world_rank[key][4] + "\n 世界ランク: " + str(world_rank[key][5]) + " / " + world_rank["現在"][0] + rep_text2 +\
+                                                rep_text = json_response["includes"]["users"][0]["name"] + "\n\n級位: " + rank + "\n⠀最高pt: " + world_rank[key][2] + "\n⠀歴代: " + str(world_rank[key][3]) + " / " + world_rank["累計"][0] + "\n⠀現在pt: " + world_rank[key][4] + "\n⠀世界ランク: " + str(world_rank[key][5]) + " / " + world_rank["現在"][0] + rep_text2 +\
                                                 "\n出場試合数: " + str(world_rank[key][7]) + "\n自己ベスト: " + world_rank[key][0] + " (" + str(world_rank[key][1]) + "回)\n戦績: 🥇×" + str(world_rank[key][8]) + " 🥈×" + str(world_rank[key][9]) + " 🥉×" + str(world_rank[key][10]) + " 📋×" + str(world_rank[key][11])
                                             else:
                                                 rep_text = json_response["includes"]["users"][0]["name"] + "\n\n最高pt: -\n歴代: - / " + world_rank["累計"][0] + "\n現在pt: -\n世界ランク: - / " + world_rank["現在"][0] + "\n出場試合数: 0\n自己ベスト: -\n戦績: 🥇×0 🥈×0 🥉×0 📋×0"
