1535ffcec1c33c19a7200e1ae8d281b089b82f74
nao-riku <76162238+nao-riku@users.noreply.github.com>
2022-09-30 04:01:12 +0900

    Update app.py

--- app.py
+++ app.py
@@ -103,8 +103,8 @@ def get_stream():
     for num in range(7):
         if com_t(times[num], now, times[num + 1]):
             start_time = datetime.datetime(times[num + 1].year, times[num + 1].month, times[num + 1].day, times[num + 1].hour, times[num + 1].minute, times[num + 1].second + 2)
-            end_time = times[num + 1]
-    start_time = datetime.datetime(now.year, now.month, now.day, now.hour, now.minute, now.second + 10)
+            end_time = times[num + 2]
+    #start_time = datetime.datetime(now.year, now.month, now.day, now.hour, now.minute, now.second) + datetime.timedelta(seconds=10)
                 
     if start_time.hour != 2:
         get_result()
